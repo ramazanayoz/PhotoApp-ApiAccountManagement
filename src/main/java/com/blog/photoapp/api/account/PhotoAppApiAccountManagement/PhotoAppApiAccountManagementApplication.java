23047317426db512115c01d20c4e421531f8c6f5
150709039 <ramazanayoz2@posta.mu.edu.tr>
2021-07-31 21:41:34 +0300

    impl

--- src/main/java/com/blog/photoapp/api/account/PhotoAppApiAccountManagement/PhotoAppApiAccountManagementApplication.java
+++ src/main/java/com/blog/photoapp/api/account/PhotoAppApiAccountManagement/PhotoAppApiAccountManagementApplication.java
@@ -2,8 +2,10 @@ package com.blog.photoapp.api.account.PhotoAppApiAccountManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotoAppApiAccountManagementApplication {
 
 	public static void main(String[] args) {
